aaf1ae3c3943149206e338bdba0bcfe64d12ec16
mizaelrodriguez <ie698323@iteso.mx>
2018-01-27 18:53:49 -0600

    4

--- source/varidic.c
+++ source/varidic.c
@@ -9,19 +9,19 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-double average(int count, ...)
+double average(int dates, ...)
 {
     va_list ap;
     int j;
     double sum = 0;
 
-    va_start(ap, count); /* Requires the last fixed parameter (to get the address) */
-    for (j = 0; j < count; j++) {
+    va_start(ap, dates); /* Requires the last fixed parameter (to get the address) */
+    for (j = 0; j < dates; j++) {
         sum += va_arg(ap, int); /* Increments ap to the next argument. */
     }
     va_end(ap);
 
-    return sum / count;
+    return sum / dates;
 }
 
 int variadic(int argc, char const *argv[])
